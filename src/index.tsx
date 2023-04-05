8a0907e25214f2f6a32f6d156aa2df207e7799c4
Jordi Bartolomé <jordi@noteflight.com>
2023-04-06 00:17:42 +0200

    Fix paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import App from "./components/App";
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from "./serviceWorker";
@@ -24,26 +23,3 @@ reportWebVitals();
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./components/App";
-// import * as serviceWorker from "./serviceWorker";
-// import { createBrowserHistory } from "history";
-// import { Router, Route } from "react-router-dom";
-
-// // Reducers
-// const history = createBrowserHistory();
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(
-//   <Router history={history}>
-//     <Route exact path="/" component={App} />
-//   </Router>,
-//   rootElement
-// );
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
