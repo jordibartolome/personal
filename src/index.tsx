5a04b56efeb52bbb51575691da82fa33f777fdc6
Jordi Bartolomé <jordi@noteflight.com>
2023-04-06 00:00:46 +0200

    Fix paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import App from "./components/App";
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,3 +19,31 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
+// import * as serviceWorker from "./serviceWorker";
+// import { createBrowserHistory } from "history";
+// import { Router, Route } from "react-router-dom";
+
+// // Reducers
+// const history = createBrowserHistory();
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   <Router history={history}>
+//     <Route exact path="/" component={App} />
+//   </Router>,
+//   rootElement
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
---
+++ src2/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import { createBrowserHistory } from "history";
+import { Router, Route } from "react-router-dom";
+
+// Reducers
+const history = createBrowserHistory();
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Router history={history}>
+    <Route exact path="/" component={App} />
+  </Router>,
+  rootElement
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
